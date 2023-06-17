4dae7efb6556e8aaa98494d177ca5cb603d56f8e
gitkazumaa <kazumaalternate@gmail.com>
2023-06-16 17:29:14 -0700

    Add functions to remove grid

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let gridContainer = document.querySelector(".grid-container");
-
-(function initializeGrid() {
-    for (let row = 0; row < 16; row++) {
-        let gridRowContainer = document.createElement("div")
-        for (let col = 0; col < 16; col++) {
+function initializeGrid(squares) {
+    let gridContainer = document.createElement("div");
+    gridContainer.setAttribute("id", "grid-container");
+    for (let row = 0; row < squares; row++) {
+        let gridRowContainer = document.createElement("div");
+        gridRowContainer.classList.add("grid-row-container");
+        for (let col = 0; col < squares; col++) {
             let square = document.createElement("div");
             square.classList.add("square");
             square.setAttribute("id", `${row}-${col}`);
@@ -14,9 +15,28 @@ let gridContainer = document.querySelector(".grid-container");
         }
         gridContainer.appendChild(gridRowContainer);
     }
-})();
+    document.body.append(gridContainer);
+};
+
+initializeGrid(16);
 
 function changeColor(id) {
     square = document.getElementById(id);
     square.classList.add("square-hover");
+}
+
+
+
+let gridSizeButton = document.querySelector(".grid-size-button");
+gridSizeButton.addEventListener("click", () => {
+    let squares = prompt("Number of Squares per side: ");
+    if (squares > 100) square = 100;
+    removeGrid();
+    initializeGrid(squares);
+});
+
+function removeGrid() {
+    let gridContainer = document.getElementById("grid-container");
+    console.log(gridContainer);
+    gridContainer.remove();
 }
